ee7416e1163680832e36bdea92227613e0fd46a5
Sathvik Nair <sathviknair@berkeley.edu>
2020-02-10 13:50:50 -0800

    bug fixes

--- codebase/automation.py
+++ codebase/automation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 from semcor_bert_pipeline import *
 from clustering import *
 
@@ -8,12 +9,16 @@ def run_clustering(word, pos, model):
     tsne_results = plot_embeddings(word_results['embeddings'], word_results['sense_indices'], word_results['sense_names'], word_results['lemma'], savefile = True)
     color_dict, label_dict = create_dendrogram_colors(word_results['sense_names'])
     plot_dendrogram(word_results, color_dict, label_dict, savefile = True)
-    raw_json = plot_gmm_rand_indices(word_results, range(2, 40))
+    raw_json = plot_gmm_rand_indices(word_results, range(2, 40), savefile = True)
+
+
 
 if __name__ == '__main__':
     model = initialize_model()
     sparse_senses = pd.read_csv('data/semcor_sparsity.csv')
-    for i in len(sparse_senses.index):
+    for i in range(20, 23):
         row = sparse_senses.iloc[i]
         word, pos = row['word'], row['pos']
+        dir_name = os.path.join("data", 'clustering_results', word + '_' + pos)
+        os.system('mkdir ' + dir_name)
         run_clustering(word, pos, model)
