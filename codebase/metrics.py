a92d435e185e8c1de1ab67ec3c21c443399a894a
Sathvik Nair <sathviknair@berkeley.edu>
2020-02-16 16:31:46 -0800

    correlation computing util

--- codebase/metrics.py
+++ codebase/metrics.py
@@ -1,5 +1,9 @@
 import numpy as np
 import torch
+import json
+import pandas as pd
+from semcor_bert_pipeline import *
+from scipy import stats
 
 def euc_dist(v1, v2):
     if type(v1) == torch.Tensor:
@@ -28,3 +32,35 @@ def cs_centroids(s1, s2):
 
 def dist_centroids(s1, s2):
     return euc_dist(centroid(s1), centroid(s2))
+
+def get_word_senses_ri(folder_name):
+    embed_fpath = os.path.join('data', 'pipeline_results', folder_name + '.json')
+    with open(embed_fpath, 'r') as embeds:
+        embed_json = json.load(embeds)
+    num_senses = len(embed_json['sense_names'])
+    gmm_fpath = os.path.join('data', 'clustering_results', folder_name, 'gmm_results.json')
+    with open(gmm_fpath, 'r') as ri:
+        ri_pca = json.load(ri)
+    return num_senses, ri_pca
+
+def get_corr_words():
+    df = pd.read_csv('data/semcor_sparsity.csv')
+    words_with_data = []
+    for i in range(len(df.index)):
+        row = df.iloc[i]
+        word, pos = row['word'], row['pos']
+        folder_name = word + '_' + pos
+        dir_name = os.path.join("data", 'clustering_results', word + '_' + pos)
+        if os.listdir(dir_name):
+            words_with_data.append(folder_name)
+    return words_with_data
+
+def compute_correlation(corr_dict, key):
+    num_senses = corr_dict[key]['num_senses']
+    gmm = corr_dict[key]['gmm_ari']
+    random = corr_dict[key]['random_ari']
+    corr_dict[key]['pearson_gmm'] = stats.pearsonr(num_senses, gmm)[0]
+    corr_dict[key]['spearman_gmm'] = stats.spearmanr(num_senses, gmm)[0]
+    corr_dict[key]['pearson_random'] = stats.pearsonr(num_senses, random)[0]
+    corr_dict[key]['pearson_random'] = stats.spearmanr(num_senses, random)[0]
+    return corr_dict
