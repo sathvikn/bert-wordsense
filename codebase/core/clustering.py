3a14f309221c9a85416e65f1228d722f088bcd14
Sathvik Nair <sathviknair@berkeley.edu>
2020-03-07 19:08:46 -0800

    module for pipeline, metrics, and clustering

---
+++ codebase/core/clustering.py
@@ -0,0 +1,203 @@
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from scipy.cluster.hierarchy import dendrogram, linkage 
+from sklearn.mixture import GaussianMixture
+from sklearn.metrics import adjusted_rand_score
+#from semcor_bert_pipeline import *
+
+def plot_embeddings(e, sense_indices, sense_names, word_name, savefile = False):
+    assert len(sense_indices) == len(sense_names)
+    as_arr = np.asarray(convert_embeddings(e))
+    dim_red = TSNE()
+    tsne_results = dim_red.fit_transform(as_arr)
+    num_senses = len(sense_indices)
+    results_for_sense = []
+
+    results_for_sense.append(tsne_results[:sense_indices[0] - 1])
+    for i in np.arange(len(sense_indices) - 1):
+        start = sense_indices[i]
+        end = sense_indices[i + 1]
+        results_for_sense.append(tsne_results[start:end])
+    
+    sense_dict = {}
+    for i in range(num_senses):
+        if i == 2:
+            plt.scatter(results_for_sense[i][:,0], results_for_sense[i][:,1], label = sense_names[i], color = 'violet')
+        else:
+            plt.scatter(results_for_sense[i][:,0], results_for_sense[i][:,1], label = sense_names[i])
+        sense_dict[sense_names[i]] = results_for_sense[i]
+
+    plt.title("BERT Embeddings for Senses of the Word \"" + word_name + "\" ")
+    plt.legend()
+    if savefile: #TODO see if we can make this a fn?
+        word_token, word_pos = get_name(word_name), get_pos(word_name)
+        path = os.path.join('data', 'clustering_results', 'tsne.png')
+        plt.savefig(path)
+        plt.clf()
+        plt.cla()
+
+    return sense_dict
+
+#clustering/viz
+def plot_dendrogram(embed_data, color_dict, label_dict, savefile = False):
+    #color_dict is of format: {sense_name: color_str...}
+    #label_dict is of format: {index: {'color': char, 'label': sense label}}
+    embeds = convert_embeddings(embed_data['embeddings'])
+    Z = linkage(embeds, method = 'single', metric = 'cosine')
+    #plt.figure(figsize = (20, 8)) # for Jupyter plotting
+    plt.figure(figsize = (9, 6)) #to plot on PDF
+    dendrogram(Z, labels = embed_data['sense_labels'], link_color_func=lambda k: 'gray')
+    ax = plt.gca()
+    xlbls = ax.get_xmajorticklabels()
+    for lbl in xlbls:
+        lbl.set_color(color_dict[lbl.get_text()])
+
+    leg_patches = [mpatches.Patch(color = label_dict[i]['color'],
+                                label = label_dict[i]['label']) for i in np.arange(len(label_dict))]
+    plt.legend(handles=leg_patches)
+    plt.title("Nearest Neighbor Dendrogram for BERT Embeddings of " + embed_data['lemma'] + " in SEMCOR")
+    if savefile:
+        word_name = embed_data['lemma']
+        word_token, word_pos = get_name(word_name), get_pos(word_name)
+        path = os.path.join('data', 'clustering_results', word_token + '_' + word_pos, 'dendrogram.png')
+        plt.savefig(path)
+        plt.clf()
+        plt.cla()
+
+def tsne_rand(pipeline_output):
+    #Only works for 2-3 components
+    results_for_word = []
+    numpy_embeds = convert_embeddings(pipeline_output['embeddings'])
+    for c in range(2, 4):
+        tsne = TSNE(n_components = c)
+        tsne_results = tsne.fit_transform(numpy_embeds)
+        true_labels = recode_labels(pipeline_output['sense_labels'])
+        num_senses = len(set(true_labels))
+        gmm_results = gmm_rand(tsne_results, num_senses, true_labels)
+        results_for_word.append({'Lemma': pipeline_output['lemma'], 'Principle Components': c,
+                                 'WordNet Mean': gmm_results['GMM'][0], 'WordNet SD': gmm_results['GMM'][1], 
+        'Random Mean': gmm_results['Random'][0],
+        'Random SD': gmm_results['Random'][1]})
+    return results_for_word
+
+def process_pca(pca_results, num_comps, word, pos):
+    results_for_word = []
+    for c in num_comps:
+        results_for_word.append({'Lemma': word + '.' + pos, 'Principle Components': c, 'WordNet Mean': np.mean(pca_results[c]['gmm_raw_aris']),
+         'WordNet SD': np.std(pca_results[c]['gmm_raw_aris']), 'Random Mean': np.mean(pca_results[c]['random_baseline_raw_ari']),
+          'Random SD': np.std(pca_results[c]['random_baseline_raw_ari'])})
+    return results_for_word
+
+def plot_pca_ev(comp_range, embeddings, lemma):
+    embeddings = np.transpose(convert_embeddings(embeddings))
+    ev_ratios = [sum(PCA(n_components = c).fit(embeddings).explained_variance_ratio_) for c in comp_range]
+    plt.plot(comp_range, ev_ratios)
+    plt.xlabel("Number of Components")
+    plt.ylabel("Variance Explained")
+    plt.title("PCA on BERT embeddings of " + lemma)
+
+def pca(embeddings, num_comps):
+    embeds = convert_embeddings(embeddings)
+    embeds = np.transpose(np.array(embeds))
+    return PCA(n_components = num_comps).fit(embeds).components_.T
+
+def plot_gmm_rand_indices(embedding_data, comp_range, save_img = False, save_json = False):
+    #Plots Rand Index means and SDs over 1000 GMM fits
+    # returns dict of format: {PCA components: {GMM ARI, Random Baseline ARI}}
+    embeddings = embedding_data['embeddings']
+    lemma = embedding_data['lemma']
+    senses = embedding_data['sense_labels']
+    num_senses = len(set(senses))
+    true_labels = recode_labels(senses)
+    gmm_wn_means = []
+    gmm_wn_sds = []
+    gmm_random_means = []
+    gmm_random_sds = []
+    raw_results = {}
+
+    for c in comp_range:
+        pca_result = pca(embeddings, c)
+        results = gmm_rand(pca_result, num_senses, true_labels)
+        gmm_wn_means.append(results['GMM'][0])
+        gmm_wn_sds.append(results['GMM'][1])
+        gmm_random_means.append(results['Random'][0])
+        gmm_random_sds.append(results['Random'][1])
+        raw_results[c] = {'gmm_raw_aris': results['gmm_raw'], 'random_baseline_raw_ari': results['random_raw']}
+    """
+    plt.errorbar(comp_range, gmm_wn_means, yerr = gmm_wn_sds, label = "WordNet Senses")
+    plt.errorbar(comp_range, gmm_random_means, yerr = gmm_random_sds, label = "Random Baseline")
+    plt.xlabel("Number of PCA Components")
+    plt.ylabel("Rand Index")
+    plt.title("Rand Indices for PCA decomposition of " + lemma)
+    plt.legend(title = 'Ground Truth')
+    """
+    word_token, word_pos = get_name(lemma), get_pos(lemma)
+    if save_img:
+        img_path = os.path.join('data', 'clustering_results', word_token + '_' + word_pos, 'gmm_evr.png')
+        plt.savefig(img_path)
+        plt.clf()
+        plt.cla()
+    if save_json:
+        json_path = os.path.join('data', 'clustering_results', word_token + '_' + word_pos, 'gmm_results.json')
+        with open(str(json_path), 'w') as path:
+            json.dump(raw_results, path)
+
+    return raw_results
+
+def recode_labels(true_labels):
+    seen = {}
+    senses_as_nums = []
+    label_num = 0
+    for l in true_labels:
+        if l not in seen:
+            seen[l] = label_num
+            label_num += 1
+        senses_as_nums.append(seen[l])
+    return senses_as_nums
+
+def gmm_rand(clustered_result, wn_labels, true_labels):
+    ari_gmm = []
+    ari_random = []
+    for _ in range(1000):
+        gmm = GaussianMixture(n_components = wn_labels)
+        gmm_preds = gmm.fit_predict(clustered_result)
+        ari_gmm.append(adjusted_rand_score(gmm_preds, true_labels))
+        random_clusters = np.random.choice(np.unique(true_labels), len(true_labels))
+        ari_random.append(adjusted_rand_score(gmm_preds, random_clusters))
+    return {'GMM': [np.mean(ari_gmm), np.std(ari_gmm)], 'Random': [np.mean(ari_random), np.std(ari_random)],
+    "gmm_raw": ari_gmm, 'random_raw': ari_random}
+
+def create_dendrogram_colors(senses):
+    color_dict = {}
+    label_dict = {}
+    for i in range(len(senses)):
+        mplotlib_color = 'C' + str(i)
+        sense = get_name(senses[i])
+        color_dict[sense] = mplotlib_color
+        label_dict[i] = {'color': mplotlib_color, 'label': sense}
+    return color_dict, label_dict
+
+def convert_embeddings(embeds):
+    if type(embeds[0]) == list:
+        embeds = [np.array(v) for v in embeds]
+    else:
+        embeds = [v.numpy() for v in embeds]
+    return embeds
+
+def get_name(lem):
+    if type(lem) != str:
+        return lem.name()
+    else:
+        return lem.split('.')[0]
+
+def get_pos(lem):
+    lem = str(lem)
+    try:
+        return lem.split('.')[1]
+    except:
+        return 'No POS'
---
+++ codebase/core/metrics.py
@@ -0,0 +1,144 @@
+import numpy as np
+import torch
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+#from semcor_bert_pipeline import *
+from scipy import stats
+from adjustText import adjust_text
+
+def euc_dist(v1, v2):
+    if type(v1) == torch.Tensor:
+        v1 = v1.numpy()
+    if type(v2) == torch.Tensor:
+        v2 = v2.numpy()
+    return np.sqrt(np.sum((v1 - v2)**2))
+
+def find_closest_distance(e1_lst, e2_lst, fn):
+    #fn can be either np.mean or minimum
+    return fn([min([euc_dist(e1, e2) for e2 in e2_lst]) for e1 in e1_lst])
+
+def centroid(arr):
+    arr = lst_to_np(arr)
+    length, dim = arr.shape
+    return np.array([np.sum(arr[:, i])/length for i in range(dim)])
+
+def lst_to_np(arr):
+    return np.array([t.numpy() for t in arr])
+
+def cosine_sim(v1, v2):
+    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+def cs_centroids(s1, s2):
+    return cosine_sim(centroid(s1), centroid(s2))
+
+def dist_centroids(s1, s2):
+    return euc_dist(centroid(s1), centroid(s2))
+
+def get_word_senses_ri(folder_name):
+    embed_fpath = os.path.join('data', 'pipeline_results', folder_name + '.json')
+    with open(embed_fpath, 'r') as embeds:
+        embed_json = json.load(embeds)
+    num_senses = len(embed_json['sense_names'])
+    gmm_fpath = os.path.join('data', 'clustering_results', folder_name, 'gmm_results.json')
+    with open(gmm_fpath, 'r') as ri:
+        ri_pca = json.load(ri)
+    return num_senses, ri_pca
+
+def check_for_embedding_data(word, pos):
+    fname = word + '_' + pos + '.json'
+    if fname in os.listdir(os.path.join('data', 'pipeline_results')):
+        return 1
+    else:
+        return 0
+
+def plot_metric_ri(x_col, df, method, dims):
+    plt.subplots()
+    plt.scatter(df[x_col], df['Random Mean_' + method], label = 'random')
+    plt.scatter(df[x_col], df['WordNet Mean_' + method], label = 'WN Senses', alpha = 0.3)
+    plt.xlabel(x_col)
+    plt.ylabel("Rand Index")
+    plt.title("Rand Scores for GMMs fitted to " + method.upper() + " Embeddings " + "vs. " + x_col + '(' + dims + ')')
+    plt.legend()
+
+def plot_dim_metric_method_combos(two_pc, three_pc, fn):
+    for d in ['2D', '3D']:
+        for m in ['pca', 'tsne']:
+            for v in ['entropy', 'num_senses']:
+                if d == '2D':
+                    fn(v, two_pc, m, d)
+                else:
+                    fn(v, three_pc, m, d)
+
+def get_sample(two_pc, three_pc, num_words):
+    two_sample = two_pc.sample(num_words)
+    three_sample = three_pc[three_pc['Lemma'].isin(two_sample['Lemma'])]
+    return two_sample, three_sample
+
+def scatter_gmm_results_text(x_col, df, method, dims):
+    #plt.scatter(ent, df['Random Mean_' + method], label = 'random')
+    x = df[x_col]
+    y = df['WordNet Mean_' + method]
+    labels = df['Lemma']
+    #plt.subplots()
+    plt.figure(figsize = (10, 8))
+    
+    plt.scatter(x, y)
+    texts = []
+    i = 0
+    for x, y, s in zip(x, y, labels):
+        texts.append(plt.text(x, y, s))
+    plt.xlabel(x_col)
+    plt.ylabel("Rand Index")
+    plt.title("Rand Scores for GMMs fitted to " + method.upper() + " Embeddings " + "vs. " + x_col + '(' + dims + ')')
+    
+    adjust_text(texts, force_points=0.2, force_text=0.2,
+           expand_points=(1, 1), expand_text=(1, 1),
+            arrowprops=dict(arrowstyle="-", color='black', lw=0.5))
+
+
+def get_corr_words():
+    df = pd.read_csv('data/semcor_sparsity.csv')
+    words_with_data = []
+    for i in range(len(df.index)):
+        row = df.iloc[i]
+        word, pos = row['word'], row['pos']
+        folder_name = word + '_' + pos
+        #fname = word + '_' + pos + '.json'
+        #dir_name = os.path.join("data", 'pipeline_results', word + '_' + pos + '.json')
+        if check_for_embedding_data(word, pos):
+            words_with_data.append(folder_name)
+    return words_with_data
+
+def compute_correlation(corr_dict, key):
+    num_senses = corr_dict[key]['num_senses']
+    gmm = corr_dict[key]['gmm_ari']
+    random = corr_dict[key]['random_ari']
+    corr_dict[key]['pearson_gmm'] = stats.pearsonr(num_senses, gmm)[0]
+    corr_dict[key]['spearman_gmm'] = stats.spearmanr(num_senses, gmm)[0]
+    corr_dict[key]['pearson_random'] = stats.pearsonr(num_senses, random)[0]
+    corr_dict[key]['spearman_random'] = stats.spearmanr(num_senses, random)[0]
+
+def plot_correlation(corr_dict, max_pcs):
+    random_sp, random_pe, gmm_sp, gmm_pe = [], [], [], []
+    for k in corr_dict:
+        pc_data = corr_dict[k]
+        random_sp.append(pc_data['spearman_random'])
+        random_pe.append(pc_data['pearson_random'])
+        gmm_sp.append(pc_data['spearman_gmm'])
+        gmm_pe.append(pc_data['pearson_gmm'])
+    num_pcs = np.arange(2, max_pcs)
+    plt.figure(figsize = (8, 6))
+    plt.subplot(1, 2, 1)
+    plt.plot(num_pcs, random_sp, label = "Random Baseline")
+    plt.plot(num_pcs, gmm_sp, label = "WordNet Senses")
+    plt.xlabel("Principle Components of BERT Embeddings")
+    plt.ylabel("Correlation Coefficient")
+    plt.title("Spearman Correlation")
+    plt.subplot(1, 2, 2)
+    plt.plot(num_pcs, random_pe, label = "Random Baseline")
+    plt.plot(num_pcs, gmm_pe, label = "WordNet Senses")
+    plt.xlabel("Principle Components of BERT Embeddings")
+    plt.ylabel("Correlation Coefficient")
+    plt.title("Pearson Correlation")
+    plt.legend()
