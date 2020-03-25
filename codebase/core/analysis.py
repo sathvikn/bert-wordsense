716db65275eb43ec5ceae0a42b9b4eb255d72d04
Sathvik Nair <sathviknair@berkeley.edu>
2020-03-25 15:40:02 -0700

    establish db connection, get df of trials

---
+++ codebase/core/analysis.py
@@ -0,0 +1,30 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import pandas as pd
+
+def access_db():
+    cred = credentials.Certificate('../data/wordsense-pilesort-firebase-adminsdk-3ipny-791a81e575.json')
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://wordsense-pilesort.firebaseio.com'
+    })
+    ref = db.reference('polysemy_pilesort')
+    return ref.get()
+
+def get_trial_data(db):
+    trials = db['trials']
+    df_rows = []
+    for trialID in trials:
+        t = trials[trialID]
+        for sense_name in t['response']:
+            left, top = segment_position_string(t['response'][sense_name])
+            row = {'trialID': trialID, 'userID': t['userID'], 'type': t['inputWord'], 'sense': sense_name, 'x': left, 'y': top}
+            df_rows.append(row)
+    return pd.DataFrame(df_rows)
+
+def segment_position_string(s):
+    left, top = s.split(";")
+    x = int(left.split(" ")[1].strip("px"))
+    y = int(top.split(" ")[1].strip("px"))
+    return x, y
+
