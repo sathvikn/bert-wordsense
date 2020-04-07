756b2f161a18ab51680c384884a12c25396810c3
Sathvik Nair <sathviknair@berkeley.edu>
2020-04-07 00:35:40 -0700

    subject index for repeat trials

--- codebase/core/analysis.py
+++ codebase/core/analysis.py
@@ -15,6 +15,7 @@ def access_db():
     ref = db.reference('polysemy_pilesort')
     return ref.get()
 
+#Returns dataframe where rows = trials
 def get_trial_data(db):
     trials = db['trials']
     df_rows = []
@@ -31,6 +32,7 @@ def get_trial_data(db):
             df_rows.append(row)
     return pd.DataFrame(df_rows)
 
+#Returns dataframe where rows = participants
 def get_participant_data(db):
     node = db['subjectInfo']
     df_rows = []
@@ -46,18 +48,7 @@ def get_participant_data(db):
         df_rows.append(row)
     return pd.DataFrame(df_rows)
 
-#Functions to calculate and plot confusion matrices from trial results
-def segment_position_string(s):
-    left, top = s.strip().split(";")
-    x = float(left.split(":")[1].strip("px").strip(" "))
-    y = float(top.split(":")[1].strip("px").strip(" "))
-    return x, y
-
-def calculate_distance(r1, r2):
-    s1 = np.array([r1['x'], r1['y']])
-    s2 = np.array([r2['x'], r2['y']])
-    return np.linalg.norm(s1 - s2)
-    
+#Represents subject's data for a word as a matrix
 def get_subject_mtx(results, userID, word_type, trial_type):
     word_data = results[(results['userID'] == userID) & (results['lemma'] == word_type) & (results['trialType'] == trial_type)]
     result_mtx = []
@@ -75,47 +66,81 @@ def get_subject_mtx(results, userID, word_type, trial_type):
     result_mtx = result_mtx / max_value
     return result_mtx, senses
 
-def plot_repeat_trials(results, userID):
+#Plots matrices for a user's repeat trials (2x2)
+def plot_repeat_trials(results, userID, subject_index = None):
     user_trials = results[results['userID'] == userID]
     repeat_types = user_trials[user_trials['trialType'] == 'repeat']['lemma'].unique()
     for l in repeat_types:
         original_result, senses = get_subject_mtx(results, userID, l, 'test')
         repeat_result, _ = get_subject_mtx(results, userID, l, 'repeat')
-        plot_mtx(original_result, senses, x = 1, y = 2, z = 1)
-        plot_mtx(repeat_result, senses, x = 1, y = 2, z = 2)
-        plt.title("Repeated Trials for " + l)
+        my_eyes = plt.figure()
+        ax1 = plt.subplot(1, 2, 1)
+        ax1.set_title("Original")
+        orig_img = ax1.imshow(original_result)
+        annotate_mtx(original_result, orig_img, ax1, senses)
+        ax2 = plt.subplot(1, 2, 2)
+        ax2.set_title("Repeat")
+        rep_img = ax2.imshow(repeat_result)
+        annotate_mtx(repeat_result, rep_img, ax2, senses)
+        pos = 2
+        my_eyes.subplots_adjust(right = pos)
+        my_eyes.suptitle("Subject " + subject_index + " Annotations for Repeated Type " + l, x = pos / 1.9)
 
-
-
-def plot_mtx(result_mtx, senses, x, y, z):
-    if x < 0 and y < 0 and z < 0:
-        fig, ax = plt.subplots()
-    else:
-        ax = plt.subplot(x, y, z)
-    im = ax.imshow(result_mtx)
-
-    # We want to show all ticks...
-    ax.set_xticks(np.arange(len(senses)))
-    ax.set_yticks(np.arange(len(senses)))
-    # ... and label them with the respective list entries
-    ax.set_xticklabels(senses)
-    ax.set_yticklabels(senses)
-    
-    threshold = im.norm(result_mtx.max())/2.
+def plot_all_repeats(results, users):
+    for u in users:
+        subject_index = str(users[users == u].index[0])
+        plot_repeat_trials(results, u, subject_index)
         
+#Plots all matrices for shared trials (words x subjects)
+def plot_all_shared(results, users):
+    shared_words = results[results['trialType'] == 'shared']['lemma'].unique()
+    user_lst = users.tolist()
+    grid = plt.GridSpec(len(shared_words), len(users), wspace = 0.2, hspace = 0.7, figure = plt.figure(figsize = (20, 20)))
+    for i in range(len(shared_words)):
+        for j in range(len(users)):
+            user_result, senses = get_subject_mtx(results, user_lst[j], shared_words[i], 'shared')
+            ax = plt.subplot(grid[i, j])
+            im = plt.imshow(user_result, aspect = 1)
+            write_text = False
+            if j == 0:
+                write_text = True
+            annotate_mtx(user_result, im, ax, senses, write_text)
 
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
+#Annotates a matrix with the senses and perceived distances
+def annotate_mtx(result_mtx, im, ax, senses, write_text = True):
+    threshold = im.norm(result_mtx.max())/2.
+
+    if write_text:
+        ax.set_xticks(np.arange(len(senses)))
+        ax.set_yticks(np.arange(len(senses)))
+
+        ax.set_xticklabels(senses)
+        ax.set_yticklabels(senses)
+
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+                 rotation_mode="anchor")
+    else:
+        ax.set_xticklabels([''] * len(senses))
+        ax.set_yticklabels([''] * len(senses))
     textcolors = ['black', 'white']
-    # Loop over data dimensions and create text annotations.
     for i in range(len(senses)):
         for j in range(len(senses)):
             square_color = textcolors[int(im.norm(result_mtx[i][j]) > threshold)]
             text = ax.text(j, i, np.round(result_mtx[i][j], 3),
                            ha="center", va="center", color=square_color)
 
-    #fig.tight_layout()
-    #plt.show()
 
+#Helper functions
 
+#Creates x and y coordinates
+def segment_position_string(s):
+    left, top = s.strip().split(";")
+    x = float(left.split(":")[1].strip("px").strip(" "))
+    y = float(top.split(":")[1].strip("px").strip(" "))
+    return x, y
+    
+#Euclidean distance
+def calculate_distance(r1, r2):
+    s1 = np.array([r1['x'], r1['y']])
+    s2 = np.array([r2['x'], r2['y']])
+    return np.linalg.norm(s1 - s2)
