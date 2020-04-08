e9d6444dea9d31256e1c84a2de84e033d55caaa0
Sathvik Nair <sathviknair@berkeley.edu>
2020-04-07 17:49:14 -0700

    MDS plots

--- codebase/core/analysis.py
+++ codebase/core/analysis.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from sklearn.manifold import MDS
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
@@ -158,3 +159,29 @@ def get_senses(db, word):
 def get_num_senses(w, db):
     return db['inputs'][w]['senses']
 
+def mean_distance_mtx(results, lemma, trial_type, user_lst):
+    shared_tensor = []
+    for j in range(len(user_lst)):
+        user_result, senses = get_subject_mtx(results, user_lst[j], lemma, trial_type)
+        if len(user_result):
+            shared_tensor.append(user_result)
+    shared_tensor = np.asarray(shared_tensor)
+    return np.mean(shared_tensor, axis = 0)
+
+def plot_mds(word_means, word, mds_model):
+    results = mds_model.fit_transform(word_means)
+    x = results[:,0]
+    y = results[:,1]
+    senses = get_senses(db, word)
+    fig, ax = plt.subplots()
+    ax.scatter(x, y)
+    for i, txt in enumerate(senses):
+        ax.annotate(txt, (x[i], y[i]))
+    plt.title("MDS over Averaged Reported Distances for " + word)
+
+def plot_all_mds(results, users, trial_type):
+    data = results[results['trialType'] == trial_type]
+    mds_model = MDS(n_components = 2, dissimilarity = 'precomputed')
+    for l in data['lemma'].unique():
+        word_means = mean_distance_mtx(results, l, trial_type, users)
+        plot_mds(word_means, l, mds_model)
