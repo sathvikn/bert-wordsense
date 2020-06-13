ddaf909dbd1118ed99b6dbbde2f51733f105af08
Sathvik Nair <sathviknair@berkeley.edu>
2020-06-13 16:48:16 -0700

    fixed script docs

--- codebase/scripts/automation.py
+++ codebase/scripts/automation.py
@@ -1,33 +1,16 @@
 import os
 import sys
+sys.path.append("..")
 import argparse
 import pandas as pd
 from core.semcor_bert_pipeline import *
 from core.clustering import *
 from fpdf import FPDF
 
-
-def run_clustering(word, pos, model, gmm_dr):
-    #either PCA or TSNE
-    print(word, pos)
-    fname = word + '_' + pos
-    word_results = run_pipeline(word, pos, model, min_senses = 10, savefile = True)
-    print("Generating Plots")
-    tsne_results = plot_embeddings(word_results['embeddings'], word_results['sense_indices'], word_results['sense_names'], word_results['lemma'], savefile = True)
-    color_dict, label_dict = create_dendrogram_colors(word_results['sense_names'])
-    plot_dendrogram(word_results, color_dict, label_dict, savefile = True)
-    print("Running GMM Simulations")
-    if gmm_dr == 'PCA':
-        raw_json = plot_gmm_rand_indices(word_results, range(2, 30), save_json = True)
-    if gmm_dr == 'TSNE':
-        return tsne_rand(word_results)
-    else:
-        tsne_rand_indices = tsne_rand(word_results)
-        rand = plot_gmm_rand_indices(word_results, range(2, 4))
-        pca_rand_indices = process_pca(rand, range(2, 4), word, pos)
-        return tsne_rand_indices, pca_rand_indices
-
 def convert_imgs_to_pdf(name, pos):
+    """
+    Reads results for type with name and PoS and compiles T-SNE and Dendrogram plots into a PDF
+    """
     pdf = FPDF('L') 
     pdf.set_auto_page_break(0)
     img_fpath = os.path.join('data', 'clustering_results', name + '_' + pos)
@@ -40,25 +23,60 @@ def convert_imgs_to_pdf(name, pos):
     print("Saving as PDF")
     pdf.output("../results/clustering_images/" + name + "_" + pos + ".pdf", "F")
 
-def run_all_sparse():
-    sparse_senses = pd.read_csv('data/semcor_sparsity.csv')
-    #be, have, see
+def run_indiv(word, pos, model):
+    """
+    Generates embeddings, T-SNE plots, and dendrograms for word, pos pairs
+    """
+    dir_name = os.path.join("data", 'clustering_results', word + '_' + pos)
+    os.system('mkdir ' + dir_name)
+    run_clustering(word, pos, model)
+    #convert_imgs_to_pdf(word, pos)
+
+def run_from_file(path, model):
+    """
+    The CSV at path should have one column for word and one column for PoS for each type.
+    This goes through all the words in the CSV and computes BERT embeddings, t-SNE plots, and dendrograms. 
+    It keeps words that BERT failed to tokenize in skipped_words.txt
+    """
+    df = pd.read_csv(path)
     skipped_words = []
-    for i in range(len(sparse_senses.index)):
-        row = sparse_senses.iloc[i]
+    for i in range(len(df.index)):
+        row = df.iloc[i]
         word, pos = row['word'], row['pos']
-        dir_name = os.path.join("data", 'clustering_results', word + '_' + pos)
+        dir_name = os.path.join('..', "data", 'clustering_results', word + '_' + pos)
         os.system('mkdir ' + dir_name)
         try: 
-            run_clustering(word, pos, model, 'GMM')
+            run_clustering(word, pos, model)
             convert_imgs_to_pdf(word, pos)
         except:
             skipped_words.append(word + '.' + pos)
             continue
-    write_to_file(skipped_words, os.path.join('data', 'skipped_sparse_words.txt'))
+    write_to_file(skipped_words, os.path.join('..', 'data', 'skipped_words.txt'))
+
+
+def run_clustering(word, pos, model, fit_gmm = False, gmm_dr = 'both'):
+    #either PCA or TSNE
+    print(word, pos)
+    fname = word + '_' + pos
+    word_results = run_pipeline(word, pos, model, min_senses = 4, savefile = True)
+    print("Generating Plots")
+    tsne_results = plot_embeddings(word_results['embeddings'], word_results['sense_indices'], word_results['sense_names'], word_results['lemma'], savefile = True)
+    color_dict, label_dict = create_dendrogram_colors(word_results['sense_names'])
+    plot_dendrogram(word_results, color_dict, label_dict, savefile = True)
+    if fit_gmm:
+        print("Running GMM Simulations")
+        if gmm_dr == 'PCA':
+            raw_json = plot_gmm_rand_indices(word_results, range(2, 30), save_json = True)
+        if gmm_dr == 'TSNE':
+            return tsne_rand(word_results)
+        else:
+            tsne_rand_indices = tsne_rand(word_results)
+            rand = plot_gmm_rand_indices(word_results, range(2, 4))
+            pca_rand_indices = process_pca(rand, range(2, 4), word, pos)
+            return tsne_rand_indices, pca_rand_indices
+
 
 def run_tsne_entropy():
-    #TODO SPLIT THIS UP
     sparse_senses = pd.read_csv('data/semcor_entropy.csv')
     completed_files = os.listdir(os.path.join('data', 'pipeline_results', 'sparse'))
     all_rand_tsne = []
@@ -136,14 +154,6 @@ def run_masc_table():
     tbl = tbl.replace({'able.n.01': 'table.n.02', 'board.n.04': 'table.n.01'}).iloc[2:]
     run_pipeline_df("table", "NOUN", tbl, model, savefile = True)   
     
-def run_test():
-    #For testing purposes, default to word table
-    word, pos = 'table', 'n'
-    dir_name = os.path.join("data", 'clustering_results', word + '_' + pos)
-    os.system('mkdir ' + dir_name)
-    run_clustering(word, pos, model, 'both')
-    #convert_imgs_to_pdf(word, pos)
-
 def run_sparse_pca():
     result_path = os.path.join('data', 'pipeline_results')
     jsons = [i for i in os.listdir(result_path) if i not in ['bat_n.json', 'table_n.json']]
@@ -157,23 +167,35 @@ def run_sparse_pca():
         
 
 def write_to_file(lst, fpath):
+    """
+    Writes each item in a list as a new line in a text file, which gets saved in fpath
+    """
     with open(fpath, 'w') as f:
         for i in lst:
             f.write(i + '\n')
 
-def check_for_embedding_data(word, pos):
+def check_for_embedding_data(word, pos, corpus = 'semcor'):
+    """
+    Returns a 1 if the embeddings for the type have been generated, 0 if not
+    """
     fname = word + '_' + pos + '.json'
-    if fname in os.listdir(os.path.join('data', 'pipeline_results')):
+    if fname in os.listdir(os.path.join('data', 'pipeline_results', corpus)):
         return 1
     else:
         return 0
 
 if __name__ == '__main__':
     model = initialize_model()
-    if sys.argv[1] == '--test':
-        run_test()
-    elif sys.argv[1] == '--pca':
-        run_all_sparse()
+    if sys.argv[1] == '--type':
+        #--type word.pos
+        word, pos = sys.argv[2].split('.')
+        run_indiv(word, pos, model)
+    elif sys.argv[1] == '--file':
+        #--file ../data/[filename].csv
+        path = sys.argv[2]
+        run_from_file(path, model)
+
+#DEPRECATED (may be worth looking at MASC stuff though)
     elif sys.argv[1] == '--tsne_entropy':
         run_tsne_entropy()
     elif sys.argv[1] == '--google_shared':
@@ -181,4 +203,4 @@ if __name__ == '__main__':
     elif sys.argv[1] == '--table_masc':
         run_masc_table()
     else:
-        "Must specify argument (one of --test, --pca, --tsne_entropy)"
+        "Must specify argument"
