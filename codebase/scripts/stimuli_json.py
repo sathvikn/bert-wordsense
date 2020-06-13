2b2271262747a4c46fdf76298ccfde4a7be5638d
Sathvik Nair <sathviknair@berkeley.edu>
2020-06-13 16:48:16 -0700

    fixed script docs

--- codebase/scripts/stimuli_json.py
+++ codebase/scripts/stimuli_json.py
@@ -6,28 +6,33 @@ import pandas as pd
 from nltk.corpus import wordnet as wn
 
 def select_clean_sentences(sentences, indices, limit = 50):
+    """
+    sentences- list of sentences in SEMCOR for a type
+    indices- list of indices for the sense
+    limit- maximum length of sentence (in words)
+    
+    Returns a randomly selected sentence from SEMCOR 
+    """
     short_sentences = [sentences[i] for i in indices if len(sentences[i].split(" ")) <= limit]
     selected = np.random.choice(short_sentences)
-    selected = re.sub(r"\s([?.!,`'](?:\s|$))", r'\1', selected)
+    selected = re.sub(r"\s([?.!,`'](?:\s|$))", r'\1', selected) #Clean out special characters
     return selected
     
 sense_as_str = lambda s: s.split('(')[1].replace(')', '')
-semcor_lemmas = pd.read_csv('../data/expt_semcor_types.csv')["Lemma"].tolist()
-with open('../data/polysemy_words.txt', 'r') as f:
+
+
+semcor_lemmas = pd.read_csv('../data/expt_semcor_types.csv')["Lemma"].tolist() #SEMCOR words based on entropy
+with open('../data/polysemy_words.txt', 'r') as f: #Types from S&R and types for shared trials
     picked_lemmas = [l.strip() for l in f.readlines()]
 lemmas = semcor_lemmas + picked_lemmas
 make_fname = lambda x: x.split('.')[0] + '_' + x.split('.')[1] + '.json'
-lemmas = [make_fname(l) for l in lemmas]
-datapath = os.path.join('..', 'data', 'pipeline_results', 'sparse')
+lemmas = [make_fname(l) for l in lemmas] 
+datapath = os.path.join('..', 'data', 'pipeline_results', 'semcor') #Location of the type's SEMCOR sentences
 inputs = {}
+
 for l in lemmas:
     with open(os.path.join(datapath, l), 'r') as f:
-        logged_data = json.load(f)
-   # print(logged_data.keys())
-   # print(len(logged_data['original_sentences']))
-   # print(logged_data['sense_names'])
-   # print(logged_data['sense_indices'])
-    
+        logged_data = json.load(f)    
     assert len(logged_data['sense_indices']) == len(logged_data['sense_names'])
     lem = logged_data['lemma']
     lem = lem.replace('.', '_') #for FB
@@ -35,6 +40,7 @@ for l in lemmas:
     for s in logged_data['sense_names']:
         l = sense_as_str(s).strip()
         #Needed to change NLTK source code for this to work
+        #strip single quotes in the synset function in nltk.corpus.reader.wordnets
         pooja_synset = wn.synset(l)
         defn = pooja_synset.definition()
         l = pooja_synset.name()
@@ -42,6 +48,7 @@ for l in lemmas:
         inputs[lem][l] = {}
         inputs[lem][l]['def'] = defn
         indices_for_sense = np.argwhere(np.array(logged_data['sense_labels']) == pooja_synset.name()).flatten()
+        #Gets indices in the list of sentences where the sense is used
         random_sentence = select_clean_sentences(logged_data['original_sentences'], indices_for_sense)
         inputs[lem][l]["sent"] = random_sentence
     inputs[lem]['senses'] = len(logged_data['sense_names'])
